306df9caf569020d358f43a1e5877a677b366cc5
Doug Harper <doug@zewkey.com>
2017-08-21 23:02:03 -0500

    Connected my label to an IBOutlet - not sure I'll need it Connected my startButton to an IBAction

--- Fartlekr WatchKit Extension/InterfaceController.swift
+++ Fartlekr WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,10 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+  @IBOutlet var titleLabel: WKInterfaceLabel!
+
+  @IBAction func startButtonTapped() {
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
+  }
 
 }

29f1e0c2b06a6e3b327a5cd760fb32cd508d7790
Doug Harper <doug@zewkey.com>
2017-08-21 22:10:27 -0500

    Initial Commit

---
+++ Fartlekr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fartlekr
+//
+//  Created by doug harper on 8/21/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

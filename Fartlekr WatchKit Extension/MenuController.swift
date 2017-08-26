3babe30e1ee0dc14c704613a307f0201de4875d5
Doug Harper <doug@zewkey.com>
2017-08-25 21:46:37 -0500

    Added a Menu controller and connected it to the startButton segue Probably just need to set the initial view to the menu, but will leave it for now. MenuController class added

---
+++ Fartlekr WatchKit Extension/MenuController.swift
@@ -0,0 +1,31 @@
+//
+//  MenuController.swift
+//  Fartlekr
+//
+//  Created by doug harper on 8/25/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class MenuController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
